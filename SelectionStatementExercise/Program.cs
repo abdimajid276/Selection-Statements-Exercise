4afc2d5b3426cbddfb826fee275f9b420ebc63f5
Abdimajid Abdikadir <abdimajid254@gmail.com>
2024-07-12 16:45:09 -0400

    started exercise

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,6 +4,10 @@
     {
         static void Main(string[] args)
         {
+            var random = new Random();
+            var favNumber:int = random.Next(1, 11);
+            int response = 0;
+            
             Console.WriteLine("Hello, World!");
         }
     }
